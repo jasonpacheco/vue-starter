85c46b229d86cda82249c30841feb0abfd0eb296
Jason Pacheco <pacheco.jason93@gmail.com>
2020-04-20 13:49:33 -0700

    Files cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,25 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    "plugin:vue/recommended", 
+    "eslint:recommended", 
+    "@vue/prettier",
+],
   parserOptions: {
     parser: "babel-eslint"
   },
   rules: {
+    'prettier/prettier': ['warn', { 'singleQuote': true, 'trailingComma': 'all' }],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": 'warn',
+    'vue/padding-line-between-blocks': 'warn',
+    'vue/html-self-closing': ['warn', {
+      'html': {
+        'void': 'always'
+      }
+    }],
+    'vue/no-unused-components': 'warn',
   }
 };
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div id="app" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: 'App',
+  components: {},
 };
 </script>
 
@@ -22,7 +15,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
 }
 </style>
